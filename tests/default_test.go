ce9fdaa6f840bd948d14053f44e5296485f40e54
mihailo <mihailo.misic@gmail.com>
2017-10-30 23:27:46 +0100

    Fixing config.yml

--- tests/default_test.go
+++ tests/default_test.go
@@ -32,6 +32,7 @@ func TestGet(t *testing.T) {
 		Convey("Status Code Should Be 200", func() {
 			So(w.Code, ShouldEqual, 200)
 		})
+
 		Convey("The Result Should Not Be Empty", func() {
 			So(w.Body.Len(), ShouldBeGreaterThan, 0)
 		})
