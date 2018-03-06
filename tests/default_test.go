af7fdd5e7efeca9e23a3f9b9f0a4f9d868fc453b
mihailomisic <mihailo.misic@gmail.com>
2018-03-06 17:48:35 +0100

    ...

--- tests/default_test.go
+++ tests/default_test.go
@@ -31,7 +31,6 @@ func init() {
 
 // TestGet is a sample to run an endpoint test
 func TestGet(t *testing.T) {
-	panic(beego.BConfig.RunMode)
 	r, _ := http.NewRequest("GET", "/v1/user", nil)
 	w := httptest.NewRecorder()
 	beego.BeeApp.Handlers.ServeHTTP(w, r)
