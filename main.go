1d55f1a7c79662ca58a59c3246a58f5662b3993a
mihailomisic <mihailo.misic@gmail.com>
2018-03-07 16:52:10 +0100

    UPDATE: Disabled circleCI testing

--- main.go
+++ main.go
@@ -1,26 +1,61 @@
 package main
 
 import (
-	_ "github.com/mihailo-misic/company-resource-api/routers"
-
-	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/orm"
-	_ "github.com/lib/pq"
+	"github.com/gin-gonic/gin"
 )
 
-func init() {
-	// Setting up Database and ORM.
-	orm.RegisterDriver("postgres", orm.DRPostgres)
-	orm.RegisterDataBase("default", "postgres", "postgres://postgres:secret@localhost/crm?sslmode=disable")
+var DB = make(map[string]string)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	// Get user value
+	r.GET("/user/:name", func(c *gin.Context) {
+		user := c.Params.ByName("name")
+		value, ok := DB[user]
+		if ok {
+			c.JSON(200, gin.H{"user": user, "value": value})
+		} else {
+			c.JSON(200, gin.H{"user": user, "status": "no value"})
+		}
+	})
+
+	// Authorized group (uses gin.BasicAuth() middleware)
+	// Same than:
+	// authorized := r.Group("/")
+	// authorized.Use(gin.BasicAuth(gin.Credentials{
+	//	  "foo":  "bar",
+	//	  "manu": "123",
+	//}))
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+		"foo":  "bar", // user:foo password:bar
+		"manu": "123", // user:manu password:123
+	}))
+
+	authorized.POST("admin", func(c *gin.Context) {
+		user := c.MustGet(gin.AuthUserKey).(string)
+
+		// Parse JSON
+		var json struct {
+			Value string `json:"value" binding:"required"`
+		}
+
+		if c.Bind(&json) == nil {
+			DB[user] = json.Value
+			c.JSON(200, gin.H{"status": "ok"})
+		}
+	})
+
+	return r
 }
 
 func main() {
-	panic(beego.BConfig.RunMode)
-	if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-	}
-	orm.RunSyncdb("default", false, true)
+	r := setupRouter()
 
-	beego.Run()
+	r.Run(":8080") // Listen and Server in localhost:8080
 }
